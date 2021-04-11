72c732639f339ed950e8ae482f8a96ead8c3f97a
TamasNyirati <tamas.nyirati@gmail.com>
2021-04-11 17:16:45 +0200

    AdatBeolvasas

--- EU/Program.cs
+++ EU/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace EU
     {
         static void Main(string[] args)
         {
+            string[] adatok = File.ReadAllLines("EUcsatlakozas.txt");
+            List<Tagallam> tagallamok = new List<Tagallam>();
+
+            foreach (string item in adatok)
+            {
+                Tagallam eutag = new Tagallam(item);
+                tagallamok.Add(eutag);
+            }
+           
+            
         }
     }
 }
