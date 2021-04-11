bc99f54714028853bf714b2235bc4ce89a3672fd
TamasNyirati <tamas.nyirati@gmail.com>
2021-04-11 16:53:48 +0200

    ures

---
+++ EU/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
